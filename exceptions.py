3388e25897929ba63d1b5777bc556099efe62deb
Rendo-M <srvr@list.ru>
2023-06-15 00:34:16 +0300

    exceptions

---
+++ exceptions.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+def main():
+    try:
+        data = input("Введите данные (в формате: Фамилия Имя Отчество dd.mm.yyyy номер_телефона пол): ").split()
+        if len(data) != 6:
+            raise ValueError("Ошибка: требуется 6 параметров")
+
+        surname, name, patronymic, dob_str, phone_str, gender = data
+        dob = datetime.strptime(dob_str, "%d.%m.%Y").date()
+        phone = int(phone_str)
+
+        if gender not in ("f", "m"):
+            raise ValueError("Ошибка: значение пола должно быть f или m")
+
+    except ValueError as e:
+        print(e)
+        return
+    except TypeError:
+        print("номер телефона должен содержать только цифры")
+        return
+    except Exception as e:
+        print("Ошибка:", e)
+        return
+
+    try:
+        with open(surname + ".txt", "a") as file:
+            file.write(surname + name + patronymic + dob_str + " " + phone_str + gender + "\n")
+        print("Данные успешно записаны в файл", surname + ".txt")
+    except Exception as e:
+        print("Ошибка при записи в файл:", e)
+
+if __name__ == "__main__":
+    main()
